de7ee7bbe8495e434900677699cb32c34cee5f41
anastasiaval <36269769+anastasiaval@users.noreply.github.com>
2019-05-07 00:14:36 +0300

    started colors component, quicksand font added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-import './style.scss';
+import 'assets/style.scss';
+import 'assets/variables.scss';
+import 'components/logo/logo.scss';
+import 'components/colors/colors.scss';
+import 'components/colors__item/colors__item.scss';
 
