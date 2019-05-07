fa025f7c25a559baead577e192b2e068b33f519f
anastasiaval <36269769+anastasiaval@users.noreply.github.com>
2019-05-08 00:55:26 +0300

    index files and global style

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import 'assets/variables.scss';
 import 'components/logo/logo.scss';
 import 'components/colors/colors.scss';
 import 'components/colors__item/colors__item.scss';
+import 'components/type/type.scss';
+import 'components/type__item/type__item.scss';
 
