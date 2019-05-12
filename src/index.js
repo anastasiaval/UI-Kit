e84abc665e6298a2f45443d84cce575ecfcfe06c
anastasiaval <36269769+anastasiaval@users.noreply.github.com>
2019-05-12 10:40:20 +0300

    updated heading mixin, added body font style, removed unnecessary css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'components/logo/logo.scss';
 import 'components/colors/colors.scss';
 import 'components/colors__item/colors__item.scss';
 import 'components/type/type.scss';
-import 'components/text/text.scss';
+import 'components/heading/heading.scss';
 import 'components/input/input.scss';
+import 'components/dropdown/dropdown.scss';
 
+import 'components/dropdown/dropdown';
