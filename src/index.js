ad587dcb1b75936ba47f31d7359edbf1bde74bed
anastasiaval <36269769+anastasiaval@users.noreply.github.com>
2019-05-28 20:30:14 +0300

    index files

--- src/index.js
+++ src/index.js
@@ -8,9 +8,14 @@ import 'components/heading/heading.scss';
 import 'components/form/form.scss';
 import 'components/input/input.scss';
 import 'components/dropdown/dropdown.scss';
+import 'components/toggle/toggle.scss';
+import 'components/radio/radio.scss';
+import 'components/checkbox/checkbox.scss';
+import 'components/like-btn/like-btn.scss';
 
 require('jquery-ui-bundle');
 import 'components/dropdown/dropdown';
 import 'components/input/input';
 import 'components/input/datepicker-ext';
+import 'components/like-btn/like-btn';
 
