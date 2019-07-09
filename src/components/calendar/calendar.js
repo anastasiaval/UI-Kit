c4d21668a681d7c5e5aadc0eb5e9ea50b9e7e8b6
anastasiaval <36269769+anastasiaval@users.noreply.github.com>
2019-07-09 19:59:18 +0300

    inline datepicker fix

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -7,3 +7,13 @@ datepicker($calendarDatepicker, calendarDatepickerOnSelect, "dd.mm.yy");
 $( "<button>", {text: 'применить', class: 'ui-datepicker-close',})
     .appendTo( $calendarDatepicker.find('.ui-datepicker-buttonpane') );
 
+$calendarDatepicker.on('click', (e) => {
+    if ($('.ui-datepicker-buttonpane').children().length === 1) {
+        $('.ui-datepicker-buttonpane').append('<button class="ui-datepicker-close">применить</button>');
+    }
+
+    if ($(e.target).text() === 'очистить') {
+        $calendarDatepicker.datepicker('setDate', [null, null]);
+    }
+});
+
